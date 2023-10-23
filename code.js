4161c0a6cae9b9f42c480165daf4e58de061e6f2
ajanu33github <146116548+ajanu33github@users.noreply.github.com>
2023-10-23 08:37:27 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,30 @@
+const screen= document.getElementById('screen');
+const buttons=document.querySelectorAll('button');
+let screenValue='';
+for(item of buttons){
+    item.addEventListener('click',(e)=>{
+        buttonText = e.target.innerText;
+        console.log('Button text is',buttonText);
+        if(buttonText=='X'){
+            buttonText  ='*';
+           screenValue+=buttonText;
+           screen.value=screenValue;
+        }
+        else if(buttonText=='AC'){
+            screenValue ="";
+        screen.value=screenValue;
+        }
+        else if(buttonText=='DE'){
+            screenValue = screenValue.slice(0,-1);
+            screen.value=screenValue;
+        }
+        else if(buttonText =='='){
+            screen.value=eval(screenValue);
+       
+        }
+        else{
+            screenValue+=buttonText;
+            screen.value=screenValue;
+        }
+    })
+}
